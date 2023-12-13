bc0f3746176eae776cadf3268fe5b082e2cb7cc4
JakeKroeger <JakyJake_Kroeger@hotmail.com>
2023-12-13 19:28:39 +0200

    Signup

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sql = require("msnodesqlv8");
 const app = express();
+const path = require("path");
+
 const port = 3000;
+function isValidUser(username, password) {
+  return new Promise((resolve, reject) => {
+    const query = `
+      SELECT * FROM login
+      WHERE email = '${username}' AND password = '${password}';
+    `;
+    sql.query(connectionString, query, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.length > 0);
+      }
+    });
+  });
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname));
 
 const connectionString =
   "server=DESKTOP-BPQEBVP;Database=pet_adoption;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
@@ -48,6 +67,49 @@ app.post("/submit", (req, res) => {
     }
   });
 });
+
+app.post("/login", (req, res) => {
+  const { signInEmail, signInPassword } = req.body;
+
+  // Your authentication logic goes here
+  isValidUser(signInEmail, signInPassword)
+    .then((userExists) => {
+      if (userExists) {
+        // User exists, redirect to cats.html
+        res.sendFile(path.join(__dirname, "index.html"));
+      } else {
+        // User not found, send an error message
+        res.status(401).send("Wrong username or password");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+app.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const query = `
+    INSERT INTO login (name, email, password)
+    VALUES ('${name}', '${email}', '${password}');
+  `;
+  sql.query(connectionString, query, (err, result) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal Server Error" });
+    } else {
+      // User registered successfully
+      res.status(200);
+      res.sendFile(path.join(__dirname, "sign in.html"));
+    }
+  });
+});
+
+// Wrap the SQL query in a promise for better handling of asynchronous operations
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
